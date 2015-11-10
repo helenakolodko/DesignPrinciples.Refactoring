23798c3b6ecf718b9405cf11b445617496350943
helenakolodko <helena.kolodko@gmail.com>
2015-11-10 22:57:59 +0300

    Added CommandFactory

---
+++ BecomeSolid.Day1/Commands/CommandFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecomeSolid.Day1
+{
+    public class CommandFactory
+    {
+        private readonly IDictionary<string, ICommand> commands;
+
+        public void Register(string commandName, ICommand command)
+        {
+            commands.Add(commandName, command);
+        }
+
+        public ICommand GetCommand(string commandName)
+        {
+            commands[commandName];
+        }
+    }
+}
--- BecomeSolid.Day1/Services/WeatherService.cs
+++ BecomeSolid.Day1/Services/WeatherService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace BecomeSolid.Day1
 {
     public class WeatherService: IService
     {
+        private string url = "http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}";
+
+        public WeatherService():
+            :this(ConfigurationManager.AppSettings["TelegramBotApiKey"])
+        {
+        }
+        public WeatherService(string apiKey) 
+        {
+            url = String.Format(url, "{0}", apiKey);
+        }
+
+       
     }
 }
